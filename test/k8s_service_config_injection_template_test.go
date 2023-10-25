4233ab44fcc8082a022507f0787ed2ecb9d54024
repo-copier <info@gruntwork.io>
2023-10-24 18:55:39 -0600

    Resolve references

--- test/k8s_service_config_injection_template_test.go
+++ test/k8s_service_config_injection_template_test.go
@@ -12,7 +12,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/gruntwork-io/terratest/modules/helm"
+	"github.com/terraform-modules-krish/terratest/modules/helm"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
