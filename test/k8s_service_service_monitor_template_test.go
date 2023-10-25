8f4a27b96a5bb12fb017dc973870032b190c877e
repo-copier <info@gruntwork.io>
2023-10-24 18:55:24 -0600

    Resolve references

--- test/k8s_service_service_monitor_template_test.go
+++ test/k8s_service_service_monitor_template_test.go
@@ -11,7 +11,7 @@ import (
 
 	promethues_operator_v1 "github.com/coreos/prometheus-operator/pkg/apis/monitoring/v1"
 	"github.com/ghodss/yaml"
-	"github.com/gruntwork-io/terratest/modules/helm"
+	"github.com/terraform-modules-krish/terratest/modules/helm"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
